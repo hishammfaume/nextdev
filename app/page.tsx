b6aa8eb8b6a1d6b5d3eea08d8a026d5621dd4eae
hishammfaume <hishammfaume@gmail.com>
2024-12-07 22:01:38 +0000

    es lint & prettier set up

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <h1 className="text-3xl text-white-700 font-black">home page</h1>
+    <h1 className="text-3xl font-black text-white ">
+      home page</h1>
    </div>
   );
-}
+} 
